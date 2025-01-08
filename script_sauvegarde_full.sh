0f8c204409d6046a667eb0a758a209d2374017d4
Suzy331 <audrey.mako@facsciences-uy1.cm>
2025-01-08 09:23:44 +0100

    Update script_sauvegarde_full.sh
    destination et utilisation de scp

--- script_sauvegarde_full.sh
+++ script_sauvegarde_full.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-SOURCE="/home/emttyconcept/Bureau/tree"
-DESTINATION="/fedoraserver"
+SOURCE="/home/emttyconcept/Bureau/Savings"
+DESTINATION="colossusmark14@192.168.56.3:/home/mes_sauvegardes"
 DATE=$(date +"%Y-%m-%d")
 
 mkdir -p "$DESTINATION"
 
-tar -czf "$DESTINATION/sauvegarde_full_$DATE.tar.gz" "$SOURCE"
+tar -czf "/tmp/sauvegarde_full_$DATE.tar.gz" "$SOURCE"
+scp "/tmp/sauvegarde_full_$DATE.tar.gz" "$DESTINATION"
+rm "/tmp/sauvegarde_full_$DATE.tar.gz"
+
 echo "Sauvegarde complète réalisée : $DESTINATION/sauvegarde_full_$DATE.tar.gz"
