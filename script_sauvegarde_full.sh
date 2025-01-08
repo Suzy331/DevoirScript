1f76a1396de59292528c3e89fc04ff69d942c204
Suzy331 <audrey.mako@facsciences-uy1.cm>
2025-01-08 10:03:36 +0100

    Update script_sauvegarde_full.sh
    mot de passe avec sshpass

--- script_sauvegarde_full.sh
+++ script_sauvegarde_full.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
+USER="colossusmark14"
+SERVER="192.168.56.3"
+PASSWORD="colossusmark"  
+
 SOURCE="/home/emttyconcept/Bureau/Savings"
-DESTINATION="colossusmark14@192.168.56.3:/home/mes_sauvegardes"
+DESTINATION="/home/mes_sauvegardes"
 DATE=$(date +"%Y-%m-%d")
+ARCHIVE_NAME="sauvegarde_full_$DATE.tar.gz"
+ARCHIVE_PATH="$DESTINATION/$ARCHIVE_NAME"
 
-mkdir -p "$DESTINATION"
 
-tar -czf "/tmp/sauvegarde_full_$DATE.tar.gz" "$SOURCE"
-scp "/tmp/sauvegarde_full_$DATE.tar.gz" "$DESTINATION"
-rm "/tmp/sauvegarde_full_$DATE.tar.gz"
+if [ ! -d "$SOURCE" ]; then
+    echo "Erreur : Le répertoire source $SOURCE n'existe pas."
+    exit 1
+fi
 
-echo "Sauvegarde complète réalisée : $DESTINATION/sauvegarde_full_$DATE.tar.gz"
+
+echo "Création de l'archive tar..."
+tar -czf "$SOURCE/$ARCHIVE_NAME" -C "$SOURCE" .
+
+
+if [ $? -ne 0 ]; then
+    echo "Erreur lors de la création de l'archive tar."
+    exit 1
+fi
+
+echo "Transfert de l'archive vers le serveur distant..."
+sshpass -p "$PASSWORD" scp "$SOURCE/$ARCHIVE_NAME" "$USER@$SERVER:$DESTINATION/"
+
+if [ $? -eq 0 ]; then
+    echo "Sauvegarde complète réalisée : $USER@$SERVER:$DESTINATION/$ARCHIVE_NAME"
+else
+    echo "Erreur lors du transfert de la sauvegarde."
+    exit 1
+fi
+
+rm "$SOURCE/$ARCHIVE_NAME"
